ac61acba7f69e810565808dc711856a598740707
kaidecoder <131601358+kaidecoder@users.noreply.github.com>
2024-01-04 15:53:30 -0500

    part 2

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+/**
+ * ! Part 1: Humble Beginnings
+**/
+
 const adventurer = {
     name: "Robin",
     health: 10,
@@ -8,9 +12,13 @@ const adventurer = {
         companion: {
             name: "Frank",
             type: "Flea",
-            belongings: ["small hat", "sunglasses"]
+            inventory: ["small hat", "sunglasses"]
         }
-    }
+    },
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a(n) ${result}.`)
+        }
     }
   
   
@@ -19,7 +27,36 @@ for (item in adventurer.inventory){
     console.log(adventurer.inventory[item])
 }
 
-//loop to access Robin's campanion info
+//loop to access Robin's companion's info
 for (item in adventurer.companion){
     console.log(adventurer.companion[item])
 }
+
+//test the dice rolls
+console.log(adventurer.roll())
+
+/**
+ * ! Part 2
+**/
+class Character{
+    constructor(name){
+        this.name = name
+        this.health = 100
+        this.inventory = []
+    }
+    roll (mod = 0) {
+        const result = Math.floor(Math.random() * 20) + 1 + mod;
+        console.log(`${this.name} rolled a(n) ${result}.`)
+        }
+}
+//create Robin
+const robin = new Character("Robin")
+robin.inventory = ["sword", "potion", "artifact"]
+robin.companion = new Character("Leo")
+robin.companion.type = "Cat"
+robin.companion.companion = new Character("Frank")
+robin.companion.companion.type = "Flea"
+robin.companion.companion.inventory = ["small hat", "sunglasses"]
+console.log()
+console.log(robin.companion.roll())
+console.log(robin.companion.companion.roll())
